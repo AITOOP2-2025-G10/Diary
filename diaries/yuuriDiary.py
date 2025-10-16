7b0e8a1e60c18781c8aed47704a0ea3bcb2e2518
yuuri-mr <k24135kk@aitech.ac.jp>
2025-10-16 16:01:38 +0900

    日記追加

---
+++ diaries/yuuriDiary.py
@@ -0,0 +1,10 @@
+from diaries.AbstractDiary import AbstractDiary
+class yuuriDiary(AbstractDiary):
+    def get_date(self):
+        return "2025-10-16"
+
+    def get_summary(self):
+        return "お腹すいた"
+
+    def get_author(self):
+        return "yuuri"
