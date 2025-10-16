141524c2b04f4a9678db254199edbb311e973500
tyamokamo <atyatya215@gmail.com>
2025-10-16 16:04:22 +0900

    日記更新
    アルトです。

---
+++ daily/ichinichi.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class DiarySample(AbstractDiary):
+
+    def get_date(self):
+        return "2025-10-16"
+    
+    def get_summary(self):
+        return "オブジェクト演習が難しい。まだよくわからないことがあった。"
+    
+    def get_author(self):
+        return "Sample"
