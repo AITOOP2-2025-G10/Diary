a5ef220d99f39d60612f3d57ecf223bda982dfc2
Rionaitkk <k24102kk@aitech.ac.jp>
2025-10-16 15:52:14 +0900

    坪内の更新日記2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
 from diaries.TsubouchiDiary import TsubouchiDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [
+    DiarySample(),
+     TsubouchiDiary(),
+]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
