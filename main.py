91380ea04ff3fbc2a5d64656448a102951868039
UNO1013 <days22194sao@au.com>
2025-10-16 15:53:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.UnoDiary import UnoDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),
+           UnoDiary(), 
+           ]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
